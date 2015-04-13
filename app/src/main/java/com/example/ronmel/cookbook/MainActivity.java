fc2e20643457033c27bd1da06e9695dad2ae66ee
Karmel <karmelc@gmail.com>
2015-04-13 21:53:50 +0300

    First try

--- app/src/main/java/com/example/ronmel/cookbook/MainActivity.java
+++ app/src/main/java/com/example/ronmel/cookbook/MainActivity.java
@@ -1,17 +1,89 @@
 package com.example.ronmel.cookbook;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+        TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+    private Button btnSpeak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tts = new TextToSpeech(this, this);
+
+        btnSpeak = (Button) findViewById(R.id.button);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+    }
+
+    private void speakOut() {
+        tts.speak("I'm a Barbie girl, in the Barbie world" +
+                "Life in plastic, it's fantastic!" +
+                "You can brush my hair, undress me everywhere" +
+                "Imagination, life is your creation" +
+                "Come on Barbie, let's go party!" +
+                "" +
+                "I'm a Barbie girl, in the Barbie world" +
+                "Life in plastic, it's fantastic!" +
+                "You can brush my hair, undress me everywhere" +
+                "Imagination, life is your creation" +
+                "" +
+                "I'm a blond bimbo girl, in a fantasy world" +
+                "Dress me up, make it tight, I'm your dolly" +
+                "You're my doll, rock'n'roll, feel the glamour in pink," +
+                "Kiss me here, touch me there, hanky panky..." +
+                "You can touch, you can play, if you say: I'm always yours", TextToSpeech.QUEUE_FLUSH, null,"1");
     }
 
 
